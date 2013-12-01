63a0e53422b6015a8d318f9f73e44ec2d23ce686
Brian Mathews <spoonart@gmail.com>
2013-11-30 17:15:53 -0800

    updating to latest text parser

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('nigelWebApp')
-  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
@@ -12,19 +12,37 @@ angular.module('nigelWebApp')
     $scope.output = [];
 
     var parser = function (message) {
-            var temp = message.toLowerCase(),
-                    results;
-            temp = temp.split("with");
-            if (temp[0]) {
-                    if (temp[1]) {
-                            results = temp[0].split(' ').slice(0, -1).join('/');
-                    } else {
-                            results = temp[0].split(' ').join('/');
-                    }
-            }
-            if (temp[1]) {
-                    temp = temp[1].split('and');
-                    temp.forEach(function (param, index) {
+        message = message.replace("search for ", "search with q as ");
+        message = message.replace("jira ", "jira with args as ");
+        var temp = message.toLowerCase(),
+                results;
+        temp = temp.split("with");
+        if (temp[0]) {
+                if (temp[1]) {
+                        results = temp[0].split(' ').slice(0, -1).join('/');
+                } else {
+        results =
+            temp[0]
+                .replace("who ", "")
+                .replace("what ", "")
+                .replace("the ", "")
+                // .replace("a ", "")
+                .replace("an ", "")
+                .replace("is ", "")
+                .replace("on ", "")
+                .replace(/[^a-zA-Z0-9-\s]+/g, "") // Replace any non word character with nothing
+                .split(' ');
+        if (temp[0].indexOf("who") === 0) {
+            // Reverse the order of the arguments so
+            // ie.  Who broke the build?
+            results = results.reverse();
+        }
+        results = results.join('/');
+                }
+        }
+        if (temp[1]) {
+                temp = temp[1].split('and');
+                temp.forEach(function (param, index) {
                         results += index ? '&' : '?';
                         var temp = param.split('as');
                         if (temp[0]) {
@@ -33,9 +51,9 @@ angular.module('nigelWebApp')
                         if (temp[1]) {
                                 results += '=' + temp[1].trim();
                         }
-                    });
-            }
-            return results || "";
+                });
+        }
+        return results || "";
     };
 
     $scope.post = function () {
